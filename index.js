44614e6bbced98d5ef469087fdbcc60c8658acba
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-09-03 00:41:03 +0700

    Add Get user location feature

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-
 mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kcm9tZWRhMHJnIiwiYSI6ImNqbGt2ZnhmZjBxeWczcHFuYzk4NzY0YW4ifQ.UzT_nIl1WQInw5KGHzEZjw';
 var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v10'
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-79.4512, 43.6568],
+    zoom: 13
 });
+
+map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+}));
